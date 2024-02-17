6f61fe4f8074d0cc22b46b8d748964d6e6a7f962
Imane SIDKI <simane1011@gmail.com>
2024-02-18 00:41:00 +0100

    chore: Changed some member variables and member functions of the channel class.

--- Channel.hpp
+++ Channel.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-#include "client.hpp"
-#include "Message.hpp"
+#include "Client.hpp"
 
 class Client;
 
@@ -9,70 +8,52 @@ enum ChannelMode {
    CHANNEL_MODE_INVITE_ONLY = 0, //+i
    CHANNEL_MODE_TOPIC_SETTABLE_BY_CHANNEL_OPERATOR_ONLY = 1, //+t
    CHANNEL_MODE_KEY = 2, //+k
-   CHANNEL_MODE_OPERATOR = 3, //+o
+   CHANNEL_MODE_OPERATOR = 3, //+o  //user mode
    CHANNEL_MODE_USER_LIMIT = 4, //+l
-};
-
-enum ClientMode {
-    CLIENT_MODE_REGULAR = 0,
-    CLIENT_MODE_OPERATOR = 1
 };
 
 class Channel {
     private:
         std::string _name;
         std::string _password;
-        Client * _creator;
-        time_t _created_at;
-        std::vector<Message *> _messages;
-        long long int _user_limit;
+        int _channel_limit;
         std::string _topic;
-        ChannelMode _mode;
         std::vector<ChannelMode> _modes;
-        std::vector<Client *> _invite_list;
-
-    public:
-        std::vector<Client *> _normal_clients;
-        std::vector<Client *> _ope_clients;
-        std::vector<std::pair<Client *, ClientMode> > _clients;
+        std::vector<Client> inviteList;
+        std::vector<Client> clientList;
+        std::vector<Client> opeList;
 
     public:
         Channel(std::string &name, std::string &password); 
         // Setters
-        void setCreator(Client *creator);
-        void setTopic(std::string &topic);
-        void setPassword(std::string passwd);
+        void setTopic(std::string &newTopic);
+        void setPassword(std::string &passwd);
+        void setChannel_limit(int limit);
         // Getters
-        std::string &getPassword(void);
-        std::string &getName(void);
-        std::string &getTopic(void);
-        Client *getCreator(void);
-        time_t getCreatedAt(void);
-        ChannelMode getMode(void);
-        std::vector<ChannelMode> getModes(void);
-        char getIdentifier(ChannelMode _mode);
-        std::string getStringModes(void);
-        size_t getUserLimit(void);
-        void setUserLimit(size_t limit);
-        std::vector<Client *> getClients(void) const;
+        std::string &getPassword(void) const;
+        std::string &getName(void) const;
+        std::string &getTopic(void) const;
+        std::vector<ChannelMode> getModes(void) const;
+        int getChannelLimit(void) const;
+        std::vector<Client> getClientList(void) const;
+        std::vector<Client> getOpeList(void) const;
+        std::vector<Client> getInviteList(void) const;
         // Client stuff
-        bool joined(Client *client);
-        bool isOwner(Client *client);
-        bool isOpe(Client *client);
-        bool isNormal(Client *client);
-        bool isInvited(Client *client);
+		void join(Client client);
+        bool isJoined(Client client);
+        bool isOpe(Client client);
+        bool isInvited(Client client);
         bool isInviteOnly(void);
-        void addOpe(Client *client);
-        void removeOpe(Client *client);
-        void kick(Client *client);
-        void invite(Client *client);
-        void removeInvite(Client *client);
+        void addOpe(Client client);
+        void removeOpe(Client client);
+        void kick(Client client);
+        void invite(Client client);
+        void removeInvite(Client client);
         // Utils
-        std::string getModeString(void);
         bool hasMode(ChannelMode mode);
         void addMode(ChannelMode mode);
         void removeMode(ChannelMode mode);
-        void removeClientFromChannel(Client *client);
         void broadcastMessage(std::string message);
         bool hasKey(void);
+        void removeKey(void);
 };
