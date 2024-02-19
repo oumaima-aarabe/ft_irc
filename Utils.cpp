3aea75486bd09d7cfd5919162eb30905da48e3f2
Imane SIDKI <simane1011@gmail.com>
2024-02-19 20:24:11 +0100

    add: Utils header for global functions

--- Utils.cpp
+++ Utils.cpp
@@ -1,6 +1,4 @@
-#include <string>
-#include <vector>
-#include "Client.hpp"
+#include "Utils.hpp"
 
 void my_trim_(std::string& s, char delimiter) {
     size_t p = s.find_first_not_of(delimiter);
@@ -10,49 +8,16 @@ void my_trim_(std::string& s, char delimiter) {
         s.erase(p + 1);
 }
 
-std::vector<std::string> split(std::string & str, std::string & delimiter) {
-    std::vector<std::string> ret;
-    my_trim_(str, ' ');
-    size_t found = str.find(delimiter);
-    if (found == std::string::npos)
-        return ret;
-    size_t i = 0;
-    while (found != std::string::npos){
-        std::string str = str.substr(i, found);
-        my_trim_(str, ' ');
-        ret.push_back(str);
-        i = found;
-        str = str.substr(found);
-        my_trim_(str, ' ');
-        found = str.find(delimiter);
+std::vector<std::string> split(std::string & str, char delimiter) {
+    std::vector<std::string> result;
+    size_t pos = 0;
+    std::string token;
+    my_trim_(str, delimiter);
+    while ((pos = str.find(delimiter)) != std::string::npos) {
+        token = str.substr(0, pos);
+        result.push_back(token);
+        str.erase(0, pos + 1);
     }
-    if (!str.empty()){
-        ret.push_back(str);
-    }
-    return (ret);
-}
-
-int  Server::parse_pair(Client &client, std::pair<std::string, std::string> pair)
-{
-  if (pair.first == "PASS")
-  {
-    if (parse_pass(client, pair.second) == -1)
-      return (-1);
-  }
-  else if (pair.first == "USER")
-  {
-    if (parse_user(client, pair.second) == -1)
-      return (-1);
-  }
-  else if (pair.first == "NICK")
-  {
-    if (parse_nick(client, pair.second) == -1)
-      return (-1);
-  }
-  else
-  {
-    std::cout << "BAD COMMAND" << std::endl;
-    return (-1);
-  }
-  return (0);
+    result.push_back(str);
+    return result;
 }
---
+++ Utils.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Client.hpp"
+
+static void my_trim_(std::string& s, char delimiter);
+static std::vector<std::string> split(std::string & str, char delimiter);
