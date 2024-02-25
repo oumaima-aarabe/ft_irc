0c1ff56b4cba2b857a3e3b14201a13d296105861
Imane SIDKI <simane1011@gmail.com>
2024-02-25 02:33:41 +0100

    fix: check if port is a number in main

--- main.cpp
+++ main.cpp
@@ -5,20 +5,26 @@ int main(int ac, char **av)
   if (ac != 3)
   {
     std::cout << "ERROR : ./exec port password" << std::endl;
-    return(0);
+    return (1);
   }
-
   std::string port = av[1];
   std::string pass = av[2];
-
-  if (!port.empty() || !pass.empty()){
-    Server server(std::stoi(port), pass);
+  int i = 0;
+  if (!port.empty() || !pass.empty())
+  {
+    while (port[i]) {
+      if (!isdigit(port[i]))
+      {
+        std::cout << "ERROR : port must be a number" << std::endl;
+        return (1);
+      }
+      i++;
+    }
+    Server server(std::atoi(port.c_str()), pass);
     server.create_server();
     server.waiting_for_connections();
+    return (0);
   }
-  else
-    std::cout << "Empty parameter\n";
-
-  return -1;
-  
+    std::cout << "ERROR: empty parameter\n";
+    return (1);  
 }
