bf66366b40ec98559063566cfe4b0aa86fd109bc
Imane SIDKI <simane1011@gmail.com>
2024-02-27 07:09:44 +0100

    add: Topic command

--- Commands/Topic.cpp
+++ Commands/Topic.cpp
@@ -1,8 +1,42 @@
 #include "../server.hpp"
 
+// TOPIC   <channel>   [ <topic> ]
 void ft_topic(commandInfo& cmd, Server& server, Client& client) {
-    //to silence the worning
-    (void) cmd;
-    (void) client;
-    (void) server;
+    if (!cmd.cmnd_args.size())
+	{
+		server.sendReply(ERR_NEEDMOREPARAMS(std::string("*"), client.nickname, cmd.cmnd_name), client.fds.fd);
+		return ;
+	}
+	if (!Channel::isValidChannelName(cmd.cmnd_args[0]))
+	{
+		server.sendReply(ERR_NOSUCHCHANNEL(std::string("*"), client.nickname, cmd.cmnd_args[0]), client.fds.fd);
+		return ;
+	}
+	std::vector<Channel>::iterator channel = server.getChannelByName(cmd.cmnd_args[0]);
+	if (channel == server.channels.end())
+	{
+		server.sendReply(ERR_NOSUCHCHANNEL(std::string("*"), client.nickname, cmd.cmnd_args[0]), client.fds.fd);
+		return ;
+	}
+	if (!channel->isJoined(client.nickname))
+	{
+		server.sendReply(ERR_NOTONCHANNEL(std::string("*"), client.nickname, channel->getName()), client.fds.fd);
+		return ;
+	}
+	if (channel->hasMode(CHANNEL_MODE_TOPIC_SETTABLE_BY_CHANNEL_OPERATOR_ONLY) && !channel->isOpe(client.nickname))
+	{
+		server.sendReply(ERR_CHANOPRIVSNEEDED(std::string("*"), client.nickname, channel->getName()), client.fds.fd);
+		return ;
+	}
+	if (cmd.cmnd_args.size() > 1) // in case of   "TOPIC <channel> :"   or   "TOPIC <channel> :<topic>"
+		channel->setTopic(cmd.cmnd_args[1]);
+	else // in case of    "TOPIC <channel>"    display the channel topic
+	{
+		server.sendReply(RPL_TOPIC(std::string("*"), client.nickname, channel->getName(), channel->getTopic()), client.fds.fd);
+		return ;
+	}
+	if (channel->getTopic().empty()) // in case of "TOPIC <channel> :" delete the channels topic (empty string)
+		channel->broadcastMessage(&client, RPL_NOTOPIC(std::string("*"), client.nickname, channel->getName()), false);
+	else
+		channel->broadcastMessage(&client, RPL_TOPIC(std::string("*"), client.nickname, channel->getName(), channel->getTopic()), false);
 }
