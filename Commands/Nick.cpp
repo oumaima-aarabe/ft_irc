888568a7032326b615b99f4ab56917d421291bb3
Imane SIDKI <simane1011@gmail.com>
2024-02-29 16:22:42 +0100

    add: Nick command

--- Commands/Nick.cpp
+++ Commands/Nick.cpp
@@ -1,9 +1,63 @@
 #include "../server.hpp"
 
-void ft_nick(commandInfo& cmd, Server& server, Client& client) {
-    //to silence the worning
-    (void) cmd;
-    (void) server;
-    (void) client;
-    return ;
+bool isValidNickChar(char c)
+{
+	return (!(c == '#' || c == ':' || c == ' '));
+}
+
+bool isValidNick(std::string &nickname)
+{
+	if (nickname.empty())
+		return (false);
+	for (size_t i = 0; i < nickname.size(); i++)
+	{
+		if (!isValidNickChar(nickname[i]))
+			return (false);
+	}
+	return (true);
+}
+
+bool isAlreadyUsed(commandInfo &cmd, Server &server)
+{
+	std::map<int, Client> users = server.users;
+
+	for (std::map<int, Client>::iterator it = users.begin(); it != users.end(); ++it)
+	{
+		if (it->second.nickname == cmd.cmnd_args[0])
+			return (true);
+	}
+	return (false);
+}
+
+void ft_nick(commandInfo& cmd, Server& server, Client& client) 
+{
+	if (!cmd.cmnd_args.size())
+	{
+		server.sendReply(ERR_NONICKNAMEGIVEN(std::string("*")), client.fds.fd);
+		return;
+	}
+	if (!isValidNick(cmd.cmnd_args[0]))
+	{
+		server.sendReply(ERR_ERRONEUSNICKNAME(std::string("*"), cmd.cmnd_args[0]), client.fds.fd);
+		return;
+	}
+	if (isAlreadyUsed(cmd, server))
+	{
+		server.sendReply(ERR_NICKNAMEINUSE(std::string("*"), cmd.cmnd_args[0]), client.fds.fd);
+		return;
+	}
+	if (!client.nickname.empty())
+	{
+		std::string oldNick = client.nickname;
+		client.nickname =cmd.cmnd_args[0];
+		std::vector<Channel> channels = server.channels;
+		for (size_t i = 0; i < channels.size(); i++)
+		{
+			if (channels[i].isJoined(client.nickname))
+				channels[i].broadcastMessage(&client, RPL_NICKCHANGE(oldNick, client.nickname), false);
+		}
+		server.sendReply(RPL_NICKCHANGE(oldNick, client.nickname), client.fds.fd);
+	}
+	else
+		client.nickname =cmd.cmnd_args[0];
 }
