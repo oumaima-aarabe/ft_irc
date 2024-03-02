33f835db5e306f3d3fcdc6c58c421566b0ba9236
kaboussi00 <kaboussi@student.1337.ma>
2024-03-02 17:16:03 +0100

    fix: Removed forbidden functions, replaced with allowed functions

--- server.cpp
+++ server.cpp
@@ -1,15 +1,16 @@
 #include "server.hpp"
+#include <sys/utsname.h>
 
-Server::Server(unsigned int port, std::string password){
-  char hostnam[256];
-  if (gethostname(hostnam, sizeof(hostnam)) == -1){
-    Logger::error("Error getting hostname");
-    return;
-  }
-  this->port = port;
-  this->password = password;
-  this->hostname = hostnam;
-}
+// Server::Server(unsigned int port, std::string password){
+//   char hostnam[256];
+//   if (gethostname(hostnam, sizeof(hostnam)) == -1){
+//     Logger::error("Error getting hostname");
+//     return;
+//   }
+//   this->port = port;
+//   this->password = password;
+//   this->hostname = hostnam;
+// }
 
 Server::~Server(){
   for(size_t i = 0; i < fds.size(); i++){
@@ -17,3 +18,14 @@ Server::~Server(){
   }
 }
 
+Server::Server(unsigned int port, std::string password) {
+    struct utsname unameData;
+    if (uname(&unameData) == -1) {
+        Logger::error("uname failed");
+        return;
+    }
+    this->port = port;
+    this->password = password;
+    this->hostname = unameData.nodename;
+    std::cout << hostname << std::endl;
+}
