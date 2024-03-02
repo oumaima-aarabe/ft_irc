bb04b2783655058145e8432e744e136708c5dcae
oumaima-aarabe <oaarabe2307@gmail.com>
2024-03-02 15:52:25 +0100

    feat : remove from op list in case leaving && todo list

--- Commands/Kick.cpp
+++ Commands/Kick.cpp
@@ -43,14 +43,6 @@ void ft_kick(commandInfo& cmd, Server& server, Client& client) {
     target->second.removeChannel(*channel);
     if ((*channel)->isOpe(cmd.cmnd_args[1]))
         (*channel)->removeOpe(cmd.cmnd_args[1]);
-	if (cmd.cmnd_args.size() > 2)
-	{
-		(*channel)->broadcastMessage(NULL, RPL_KICK(setPrefix(server.hostname, client.nickname, client.realname), cmd.cmnd_args[0], cmd.cmnd_args[1], cmd.cmnd_args[2]), false);
-		server.sendReply(RPL_KICK(":" + client.nickname, cmd.cmnd_args[0], cmd.cmnd_args[1], cmd.cmnd_args[2]), target->second.fds.fd);
-	}
-	else
-	{
-		(*channel)->broadcastMessage(NULL, RPL_KICK(setPrefix(server.hostname, client.nickname, client.realname), cmd.cmnd_args[0], cmd.cmnd_args[1], ""), false);
-		server.sendReply(RPL_KICK(":" + client.nickname, cmd.cmnd_args[0], cmd.cmnd_args[1], ""), target->second.fds.fd);
-	}
+	(*channel)->broadcastMessage(NULL, RPL_KICK(setPrefix(server.hostname, client.nickname, client.realname), cmd.cmnd_args[0], cmd.cmnd_args[1], ((cmd.cmnd_args.size() > 2) ? cmd.cmnd_args[2] : "")), false);
+	server.sendReply(RPL_KICK(":" + client.nickname, cmd.cmnd_args[0], cmd.cmnd_args[1], ((cmd.cmnd_args.size() > 2) ? cmd.cmnd_args[2] : "")), target->second.fds.fd);
 }
